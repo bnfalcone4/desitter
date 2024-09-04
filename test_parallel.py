ec8a708abc13449130d48feb95b8f72b37c9efc9
Bruno Falcone <bnfalcone@yahoo.es>
2024-09-04 15:56:07 +0100

    Loop

--- test_parallel.py
+++ test_parallel.py
@@ -1,41 +1,25 @@
 from multiprocessing import Pool, get_context
 
 
-def return_x(x,y):
+def return_x(x):
 
-	return x
+    suma = 0
 
-x_list = [(x,x) for x in range(100)]
-args = x_list
+    for i in range(100):
+        for j in range(100):
+            for k in range(100):
+                suma += i+j+k
 
-with Pool(8) as p:
+    return suma
 
-    extended = p.starmap(return_x, args)
+if __name__ == 'name':
 
-print(extended)
+    x_list = [(x) for x in range(100)]
+    args = x_list
+    print(x_list)
 
+    with Pool(8) as p:
 
-"""
-#!/usr/bin/env python3
-from functools import partial
-from itertools import repeat
-from multiprocessing import Pool, freeze_support
+        extended = p.starmap(return_x, args)
 
-def func(a, b):
-    return a + b
-
-def main():
-    a_args = [1,2,3]
-    second_arg = 1
-    with Pool() as pool:
-        L = pool.starmap(func, [(1, 1), (2, 1), (3, 1)])
-        M = pool.starmap(func, zip(a_args, repeat(second_arg)))
-        N = pool.map(partial(func, b=second_arg), a_args)
-        assert L == M == N
-
-    print(L)
-
-if __name__=="__main__":
-    #freeze_support()
-    main()
-"""
+    print(extended)
